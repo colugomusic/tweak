6951ffba6b06f7dc29be4b2717679ba78dcb59b9
Colugo <68328892+colugomusic@users.noreply.github.com>
2021-09-20 20:24:15 +0100

    Add ms

--- include/tweak/std.hpp
+++ include/tweak/std.hpp
@@ -167,5 +167,27 @@ inline auto from_string(const ::std::string& str)
 
 } // percentage_bipolar
 
+namespace ms {
+
+inline auto stepify(float v)
+{
+	return math::stepify(v, 0.001f);
+}
+
+inline auto to_string(float v)
+{
+	::std::stringstream ss;
+
+	ss << ms::stepify(v) << " ms";
+
+	return ss.str();
+}
+
+inline auto from_string(const ::std::string& str)
+{
+	return tweak::find_positive_number<float>(str);
+};
+
+} // ms
 } // std
 } // tweak
